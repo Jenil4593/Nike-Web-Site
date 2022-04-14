a485994c63b130510163f53b9be281d1b4bed1bf
Jenil Thakor <85248034+Jenil4593@users.noreply.github.com>
2022-04-14 19:56:20 +0530

    Add files via upload

---
+++ shoes.js
@@ -0,0 +1,10 @@
+function changebackground(color)
+{
+    const body = document.querySelector(".bb");
+    body.style.background = color;
+}
+
+function imgchange(image)
+{
+    document.querySelector(".shoes").src = image;
+}
